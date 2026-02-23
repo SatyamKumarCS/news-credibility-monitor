42346c88df95b2000cfeb5fbf91d09df0eaddedf
SatyamKumarCS <satyamkumarch15@gmail.com>
2026-02-23 11:29:13 +0530

    Feat(add): Adding UI

--- src/utils/text_cleaner.py
+++ src/utils/text_cleaner.py
@@ -10,7 +10,12 @@ except:
     stop_words = set(stopwords.words("english"))
 
 def clean_text(text):
-    text = str(text).lower()
+    text = str(text)
+
+    text = re.sub(r'^.*?\([\w\s]+\)\s*[-—]\s*', '', text)
+
+    # Standard cleaning
+    text = text.lower()
     text = re.sub(r"[^a-zA-Z\s]", "", text)
     words = text.split()
     words = [w for w in words if w not in stop_words]
