30e761125fe6b825308ea6580feebe3cf601f639
SatyamKumarCS <satyamkumarch15@gmail.com>
2026-02-23 10:41:51 +0530

    Feat(add): Adding Models File

---
+++ src/models/evaluate.py
@@ -0,0 +1,8 @@
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
+    print("\nClassification Report:\n", classification_report(y_test, y_pred))
