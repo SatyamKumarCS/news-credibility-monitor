540b7d9a9afbff429054bb3ff23bd2cb4eb491e7
SatyamKumarCS <satyamkumarch15@gmail.com>
2026-02-23 10:42:25 +0530

    Feat(add): Adding Utils File

---
+++ src/utils/text_cleaner.py
@@ -0,0 +1,17 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+
+# Ensure stopwords are available
+try:
+    stop_words = set(stopwords.words("english"))
+except:
+    nltk.download("stopwords")
+    stop_words = set(stopwords.words("english"))
+
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
+    words = text.split()
+    words = [w for w in words if w not in stop_words]
+    return " ".join(words)
